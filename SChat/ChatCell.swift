b40c543a765acf573684e6ae86cbe78e9f2688d9
exstalis <exstalis@gmail.com>
2016-12-21 16:56:13 +0300

    Keyboard controls and layout initials

---
+++ SChat/ChatCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatCell.swift
+//  SChat
+//
+//  Created by elif ece arslan on 12/21/16.
+//  Copyright © 2016 KolektifLabs. All rights reserved.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
